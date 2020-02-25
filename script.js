4306ecc0337185c60a9fdf80b7577ddec3141145
Joke Heyndels <jo_hannah@outlook.com>
2020-02-25 10:45:39 +0100

    Shorten script with a forEach loop

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 const page = document.querySelector("body");
 const pressed = [];
 const secretCode = "hufflepuff";
+const houses = ["hufflepuff", "ravenclaw", "gryffindor", "slytherin"]
 window.addEventListener("keyup", e => {
   pressed.push(e.key);
   pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  if (pressed.join("").includes("hufflepuff")) {
-    console.log("Honesty before glory");
-    page.classList.remove("slytherin", "gryffindor", "ravenclaw");
-    page.classList.add("hufflepuff");
-  } else if (pressed.join("").includes("ravenclaw")) {
-    console.log("Wit beyond measure is man's greatest treasure");
-    page.classList.remove("hufflepuff", "gryffindor", "slytherin");
-    page.classList.add("ravenclaw");
-  } else if (pressed.join("").includes("gryffindor")) {
-    console.log("GO GO GRYFFINDOR");
-    page.classList.remove("hufflepuff", "slytherin", "ravenclaw");
-    page.classList.add("gryffindor");
-  } else if (pressed.join("").includes("slytherin")) {
-    console.log("");
-    page.classList.remove("hufflepuff", "gryffindor", "ravenclaw");
-    page.classList.add("slytherin");
+  houses.forEach(house => {
+    console.log(pressed.join)
+    if (pressed.join("").includes(house)) {
+      houses.forEach(house => {
+        page.classList.remove(house);
+      })    
+    page.classList.add(house);
   } else {
-    console.log("Draco dormiens nunquam titillandus");
-    page.classList.remove("hufflepuff", "gryffindor", "ravenclaw", "slytherin");
+    page.classList.remove(houses.forEach(house => house))
   }
-});
+})});
